d966968759855e3fc58a0f2cd8653e850e844d46
Martin Emmert <martin@martinemmert.de>
2020-08-31 19:46:17 +0200

    feat(Editor): implement adding and editing bullets
    - users can add a bullet point via add button
    - users can add a new bullet point by pressing enter
    - users can stop editing a bullet point by pressing escape
    - users can edit a bullet point by pressing on it
    
    resolves #2

--- src/app/__tests__/adding-and-editing-bullets.spec.tsx
+++ src/app/__tests__/adding-and-editing-bullets.spec.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Editor } from "../Editor";
-import { render, cleanup, act } from "@testing-library/react";
+import { render, cleanup, act, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe("As a user, I want to write down my thoughts in a bullet list so that I can see them all in one place", () => {
@@ -33,14 +33,113 @@ describe("As a user, I want to write down my thoughts in a bullet list so that I
     expect(previousDocumentNode).toBeInTheDocument();
     expect(previousDocumentNode).not.toBeInstanceOf(HTMLInputElement);
     // The previous generated elements should not be in de DOM anymore
-    expect(newDocumentNode).not.toBeInTheDocument();
     expect(newDocumentNodeInput).not.toBeInTheDocument();
     expect(nextDocumentNode).toBeInTheDocument();
     expect(nextDocumentNodeInput).toBeInTheDocument();
     expect(nextDocumentNodeInput).toHaveFocus();
   });
 
-  it.todo("should, by pressing enter, stop the edit mode of the current edited bullet point, create a new one and enter its edit mode and focus on it")
-  it.todo("should, by pressing escape or blur the current node, stop its edit mode");
+  it("should, by pressing enter, stop the edit mode of the current edited bullet point, create a new one and enter its edit mode and focus on it", async () => {
+    const screen = render(<Editor />);
+    const button = screen.getByText("Add");
+    // Step 1: Click the add button
+    act(() => { userEvent.click(button) });
+    const newDocumentNode = screen.getByTestId("new-document-node");
+    const newDocumentNodeInput = screen.getByTestId("new-document-node-input");
+    expect(newDocumentNode).toBeInTheDocument();
+    expect(newDocumentNodeInput).toBeInTheDocument();
+    expect(newDocumentNodeInput).toHaveFocus();
+    await act(() => userEvent.type(newDocumentNodeInput, "Do what you love!"));
+    // Step 2: Click the add button again
+    act(() => {
+      fireEvent.keyDown(newDocumentNodeInput, { key: 'Enter', code: 'Enter' });
+    });
+    const previousDocumentNode = screen.getByText("Do what you love!");
+    const nextDocumentNode = screen.getByTestId("new-document-node");
+    const nextDocumentNodeInput = screen.getByTestId("new-document-node-input");
+    // The thext of the previous generated bullet point should be rendered not through an input field
+    expect(previousDocumentNode).toBeInTheDocument();
+    expect(previousDocumentNode).not.toBeInstanceOf(HTMLInputElement);
+    // The previous generated elements should not be in de DOM anymore
+    expect(newDocumentNodeInput).not.toBeInTheDocument();
+    expect(nextDocumentNode).toBeInTheDocument();
+    expect(nextDocumentNodeInput).toBeInTheDocument();
+    expect(nextDocumentNodeInput).toHaveFocus();
+  });
+
+  it("should, by pressing escape, stop the edit mode of the current edited bullet point", async () => {
+    const screen = render(<Editor />);
+    const button = screen.getByText("Add");
+    // Step 1: Click the add button
+    act(() => { userEvent.click(button) });
+    const newDocumentNode = screen.getByTestId("new-document-node");
+    const newDocumentNodeInput = screen.getByTestId("new-document-node-input");
+    expect(newDocumentNode).toBeInTheDocument();
+    expect(newDocumentNodeInput).toBeInTheDocument();
+    expect(newDocumentNodeInput).toHaveFocus();
+    await act(() => userEvent.type(newDocumentNodeInput, "Do what you love!"));
+    // Step 2: Click the add button again
+    act(() => {
+      fireEvent.keyDown(newDocumentNodeInput, { key: 'Escape', code: 'Escape' });
+    });
+    const previousDocumentNode = screen.getByText("Do what you love!");
+    // The thext of the previous generated bullet point should be rendered not through an input field
+    expect(previousDocumentNode).toBeInTheDocument();
+    expect(previousDocumentNode).not.toBeInstanceOf(HTMLInputElement);
+    expect(() => {
+      screen.getByTestId("new-document-node");
+    }).toThrow();
+    expect(() => {
+      screen.getByTestId("new-document-node-input");
+    }).toThrow();
+  });
+
+  it("should, by removing the focus, stop the edit mode of the current edited bullet point", async () => {
+    const screen = render(<Editor />);
+    const button = screen.getByText("Add");
+    // Step 1: Click the add button
+    act(() => { userEvent.click(button) });
+    const newDocumentNode = screen.getByTestId("new-document-node");
+    const newDocumentNodeInput = screen.getByTestId("new-document-node-input");
+    expect(newDocumentNode).toBeInTheDocument();
+    expect(newDocumentNodeInput).toBeInTheDocument();
+    expect(newDocumentNodeInput).toHaveFocus();
+    await act(() => userEvent.type(newDocumentNodeInput, "Do what you love!"));
+    // Step 2: Click the add button again
+    act(() => {
+      userEvent.click(screen.container);
+    });
+    const previousDocumentNode = screen.getByText("Do what you love!");
+    // The thext of the previous generated bullet point should be rendered not through an input field
+    expect(previousDocumentNode).toBeInTheDocument();
+    expect(previousDocumentNode).not.toBeInstanceOf(HTMLInputElement);
+    expect(() => {
+      screen.getByTestId("new-document-node");
+    }).toThrow();
+    expect(() => {
+      screen.getByTestId("new-document-node-input");
+    }).toThrow();
+  });
+
+  it("should, by clicking on an existing bullet point, enter its edit mode and store any changes made to the bullet point", async () => {
+    const screen = render(<Editor />);
+    act(() => { userEvent.click(screen.getByText("Add")) });
+    await act(() => userEvent.type(screen.getByTestId("new-document-node-input"), "Do what you love!"));
+    act(() => { userEvent.click(screen.getByText("Add")) });
+    await act(() => userEvent.type(screen.getByTestId("new-document-node-input"), "A day has 24 hours, every day."));
+    act(() => {
+      userEvent.click(screen.getByText("Do what you love!"));
+    });
+    const input = await screen.findByDisplayValue("Do what you love!");
+    expect(input).toBeInstanceOf(HTMLInputElement);
+    expect(input).toHaveFocus();
+    await act(() => userEvent.type(input, "{backspace} every day!"));
+    userEvent.click(screen.container);
+    expect(() => {
+      screen.getByText("Do what you love!");
+    }).toThrow();
+    expect(screen.getByText("Do what you love every day!")).toBeInTheDocument();
+  })
+
   it.todo("should give each bullet point an unique id upon creation");
 });
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import "../styles/main.css";
+// import "../styles/main.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
